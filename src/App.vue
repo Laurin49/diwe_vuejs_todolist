5a201b2f89fbb3cca8e792211861241a1c144458
Laurin49 <welter@diwe.info>
2023-11-15 10:43:40 +0100

    dev0 setup + first component

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { computed, ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
+import TodoItemsCategory from "./components/TodoItemsCategory.vue";
+
+const todoItems = ref([
+  {
+    id: uuidv4(),
+    taskName: "First Tode Item",
+    description: "Description 1",
+    dueDate: "2022-09-09",
+    done: false
+  },
+  {
+    id: uuidv4(),
+    taskName: "Second Tode Item",
+    description: "Description 2",
+    dueDate: "2022-09-10",
+    done: true
+  },
+  {
+    id: uuidv4(),
+    taskName: "Third Tode Item",
+    description: "Description 3",
+    dueDate: "2022-09-11",
+    done: false
+  }
+]);
+
+const todoItemsNotDone = computed(() => {
+  return todoItems.value.filter((item) => !item.done)
+});
+const todoItemsDone = computed(() => {
+  return todoItems.value.filter((item) => item.done)
+});
+const todoItemsNotDoneCategoryTitle = computed(
+  () => `Upcoming - (${todoItemsNotDone.value.length})`
+);
+
+const todoItemsDoneCategoryTitle = computed(
+  () => `Done - (${todoItemsDone.value.length})`
+);
+</script>
+
+<template>
+  <header class="colored-header"></header>
+  <main>
+    <h1 class="title">TodoList</h1>
+    <TodoItemsCategory :title="todoItemsNotDoneCategoryTitle" :todo-items="todoItemsNotDone"/>
+    <TodoItemsCategory :title="todoItemsDoneCategoryTitle" :todo-items="todoItemsDone"/>
+  </main>
+</template>
+
+<style scoped>
+.colored-header {
+  min-height: 5vh;
+  min-width: 100vw;
+  background: var(--background-light);
+}
+.title {
+  font-size: 2rem;
+  font-weight: 600;
+  color: #fff;
+}
+@media (min-width: 600px) {
+  main {
+    padding: 0 20vw;
+  }
+}
+</style>
---
+++ src/components/TodoItemsCategory.vue
@@ -0,0 +1,19 @@
+<script setup>
+    defineProps(["title", "todoItems"]);
+</script>
+
+<template>
+    <h3 class="category-title">{{ title }}</h3>
+    <p style="color: white">{{ todoItems }}</p>
+</template>
+
+<style scoped>
+.category-title {
+    color: var(--text-color-light);
+    font-weight: 600;
+  }
+  
+  .todo-item {
+    margin-bottom: 16px;
+  }
+</style>
